3822b83cc0bec1913de99995cb4703ebea7295f1
Vikas <sharmavikas12909@gmail.com>
2026-05-27 14:51:48 +0530

    feat: configure filesystem disks and customize application public path in bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -26,3 +26,11 @@ return Application::configure(basePath: dirname(__DIR__))
         __DIR__.'/../app/Listeners',
     ])
     ->create();
+
+$publicPath = file_exists(dirname(__DIR__).'/../htdocs')
+    ? dirname(__DIR__).'/../htdocs'
+    : dirname(__DIR__).'/public';
+
+$app->usePublicPath($publicPath);
+
+return $app;
